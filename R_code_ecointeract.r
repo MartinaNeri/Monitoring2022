dcfa04fd0ba11f2ff44bad6b03356b68185a1c95
MartinaNeri <114755891+MartinaNeri@users.noreply.github.com>
2022-10-15 13:17:36 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -1,2 +1,17 @@
 # Code for investigating relatioships among ecological variable
+
+#per installare un pacchetto install.packagess("")
+#per richiamare un pacchetto library()
 library(sp)
+summary(dataset)
+
+> plot(meuse$cadmium, meuse$zink)
+> 
+> cad<-meuse$cadmium
+> z<- meuse$zink
+> plot(cad,z)
+> 
+> attach(meuse)
+> detach(meuse)
+
+pairs(meuse)
