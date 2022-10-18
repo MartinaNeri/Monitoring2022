5e596dfc95a995b16fa861b6e8cd55db727d8f2b
MartinaNeri <114755891+MartinaNeri@users.noreply.github.com>
2022-10-18 09:23:41 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -1,6 +1,7 @@
 # Code for investigating relatioships among ecological variable
 
 #per installare un pacchetto install.packagess("")
+#le virgolette servono a richiamare qualcosa da "fuori"
 #per richiamare un pacchetto library()
 library(sp)
 summary(dataset)
@@ -15,3 +16,29 @@ summary(dataset)
 > detach(meuse)
 
 pairs(meuse)
+
+
+
+
+
+LECTURE 3
+> library(sp)
+Messaggio di avvertimento:
+il pacchetto ‘sp’ è stato creato con R versione 4.1.3 
+> data(meuse)
+> c<-meuse$cadmium
+> z<- meuse$zink
+> plot(c,z, col="blue", cex=1)
+> 
+
+
+> pair(meuse[,3:6]
++ pair(meuse[,3:6])
+Errore: simbolo inatteso in:
+"pair(meuse[,3:6]
+pair"
+> pair(meuse[,3:6])
+Errore in pair(meuse[, 3:6]) : non trovo la funzione "pair"
+> pairs(meuse[,3:6])
+> pairs(~ cadmium + copper + lead + zinc, data=meuse)
+> 
