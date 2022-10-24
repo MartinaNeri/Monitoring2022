d866efa0bdc492faa386902013ec2ae394ae6625
MartinaNeri <114755891+MartinaNeri@users.noreply.github.com>
2022-10-24 17:03:05 +0200

    Update R_code_ggplot2.r

--- R_code_ggplot2.r
+++ R_code_ggplot2.r
@@ -1,16 +1,15 @@
 ###### Installing the ggplot2 library / package
 
 install.packages("ggplot2")
-
 library(ggplot2)
 
-virus<- c(10, 30, 40, 50,60,80)
+virus<- c(10, 30, 40, 50,60,80) # array of values: c
 death<- c(100, 240, 310, 470, 580, 690)
 
 plot(virus, death, pch=19)
 
+# dataframe
 data.frame(virus, death)
-
 #  virus death
 #1    10   100
 #2    30   240
@@ -23,18 +22,33 @@ d<- data.frame(virus, death)
 ggplot(d, x=virus, y=death)
 ggplot(d, aes(x=virus, y=death))+geom_point()
 
-# point+ lines
+# for lines use --> + lines geom_line() 
+
+# for points + line use --> + geom_point(size = 5, col = "red")  +  geom_line()
 ggplot(d, aes(x=virus, y=death))+geom_point(size=3, col="cyan 3", pch=17)+ geom_line(col="black")
 
-# polygons
+# for polygons use--> + geom_polygon()
+
 ggplot(d, aes(x = virus, y = death)) + geom_polygon()
-geom_polygon(d, aes(x = virus, y = death), col="cyan 3")
 
 21/10 LECTURE
 
 library(ggplot2)
+
+# setwd for Windows
+# setwd("C:/lab/") , remember to use the backslash
 setwd("C:/Users/Martina/Desktop/Monitoring Ecosystem Changes and Functioning/R")
+
 covid <- read.table("covid_agg.csv", header=TRUE)
 covid
+
 summary(covid)
+
+# ggplot2
+library(ggplot2)
+ggplot(covid, aes(x = lon, y = lat)) + geom_point()
+
+# changing the size of the data...
+ggplot(covid, aes(x = lon, y = lat, size = cases)) + geom_point()
+
 
